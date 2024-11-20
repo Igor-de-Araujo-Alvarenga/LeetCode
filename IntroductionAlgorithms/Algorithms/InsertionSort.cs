98f5ec03e70548c8dbba2a4297d20117d4e7c36b
igor <igor.alvarenga@compuletra.com.br>
2024-11-20 18:05:23 -0300

    implementing sort by desc

--- IntroductionAlgorithms/Algorithms/InsertionSort.cs
+++ IntroductionAlgorithms/Algorithms/InsertionSort.cs
@@ -31,5 +31,21 @@ namespace IntroductionAlgorithms.Algorithms
             }
             return input;
         }
+
+        public int[] SortDesc(int[] input)
+        {
+            for (int j = 1; j < input.Length; j++)
+            {
+                int key = input[j];
+                int i = j - 1;
+                while (i >= 0 && input[i] < key)
+                {
+                    input[i + 1] = input[i];
+                    i = i - 1;
+                }
+                input[i + 1] = key;
+            }
+            return input;
+        }
     }
 }
--- IntroductionAlgorithms/Program.cs
+++ IntroductionAlgorithms/Program.cs
@@ -9,6 +9,7 @@ namespace IntroductionAlgorithms
             var sort = new InsertionSort();
 
             var sorted = sort.Sort(new int[] {10,9,8,7,6,5,4,3,2,1} );
+            var sortedDesc = sort.SortDesc(new int[] { 4, 8, 1, 20 });
         }
     }
 }
